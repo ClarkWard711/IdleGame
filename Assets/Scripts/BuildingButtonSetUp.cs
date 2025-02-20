5bb86a53333476fa3dac7cb098b01aff75225296
Clark Ward <67551381+ClarkWard711@users.noreply.github.com>
2025-02-21 00:22:36 +0800

    0.0。6
    完善按钮的部分配置
    增加cost prefab

--- Assets/Scripts/BuildingButtonSetUp.cs
+++ Assets/Scripts/BuildingButtonSetUp.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingButtonSetUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public Text Name, Description;
+	public Image BuildingSprite;
+	public GameObject CostPrefab, Content;
+	public int ID;
+	private void Awake()
+	{
+		gameObject.GetComponent<Button>().onClick.AddListener(OnClick);
+	}
+	public void SetUp(BuildingSO buildingSO)
+	{
+		Name.text = buildingSO.BuildingName;
+		Description.text = buildingSO.BuildingDescription;
+		BuildingSprite.sprite = buildingSO.BuildingSprite;
+		//GameObject cost = Instantiate(CostPrefab, Content.transform);
+	}
+	public void OnClick()
+	{
+		BuildingController.Instance.BuildingIDOnLoad = ID;
+		BuildingController.Instance.isGoingToBuild = true;
+	}
 }
--- Assets/Scripts/BuildingController.cs
+++ Assets/Scripts/BuildingController.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class BuildingController : MonoBehaviour
 {
+	public static BuildingController Instance;
 	public GameObject ButtonPrefab, Content;
 	public BuildingConfigSO buildingConfigSO;
+	public int BuildingIDOnLoad = -1;
+	public bool isGoingToBuild = false;
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+	}
+
 	private void Start()
 	{
 
